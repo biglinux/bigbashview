9e12204bd1d82006d07ceb124081f35a40b022a1
Bruno Goncalves <bigbruno@gmail.com>
2020-11-14 11:51:29 -0300

    Change variable to dont use p_

--- bigbashview/usr/share/bigbashview/bbv/server/utils.py
+++ bigbashview/usr/share/bigbashview/bbv/server/utils.py
@@ -2,10 +2,10 @@ def to_s(text):
     return text if isinstance(text, str) else text.decode("utf-8")
 
 
-def get_env_for_shell(query, prefix='p_'):
+def get_env_for_shell(query):
 
     def join_options(opt):
-        name = prefix+to_s(opt[0])
+        name = to_s(opt[0])
         values = ";".join([to_s(x).replace(';', '\\;') for x in opt[1]])
         return name, values
 
