070bf5b107b0728830b1ae6dea597236b4150ab0
vcatafesta <vcatafesta@gmail.com>
2023-08-04 06:49:24 -0400

    sex 04 ago 2023 06:49:24 -04 Vilmar Catafesta (vcatafesta@gmail.com)

--- bigbashview/usr/share/bigbashview/bcc/shell/bcclib.sh
+++ bigbashview/usr/share/bigbashview/bcc/shell/bcclib.sh
@@ -33,12 +33,10 @@
 #  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 #  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-readonly APP="${0##*/}"
-readonly _VERSION_="1.0.0-20230726"
-readonly red=$(tput setaf 124)
-readonly green=$(tput setaf 2)
-readonly pink=$(tput setaf 129)
-readonly reset=$(tput sgr0)
+red=$(tput setaf 124)
+green=$(tput setaf 2)
+pink=$(tput setaf 129)
+reset=$(tput sgr0)
 
 function sh_debug {
 	export PS4='${red}${0##*/}${green}[$FUNCNAME]${pink}[$LINENO]${reset} '
@@ -83,6 +81,7 @@ function sh_getbgcolor {
 		echo '<body>'
 	fi
 }
+export -f sh_getbgcolor
 
 function sh_setbgcolor {
 	local cfile="$HOME/.config/bigbashview_lightmode"
@@ -91,45 +90,55 @@ function sh_setbgcolor {
 	[[ "$1" = "true" ]] && lightmode=1
 	echo "$lightmode" >"$cfile"
 }
+export -f sh_setbgcolor
 
 function sh_lang_he {
 	grep ^he <<<"$LANG"
 	return "$?"
 }
+export -f sh_lang_he
 
 function sh_getcpu {
 	#awk -F ':' 'NR==1 {print $2}' <<< "$(grep 'model name' /proc/cpuinfo)"
 	grep 'model name' /proc/cpuinfo | awk -F ':' 'NR==1 {print $2}'
 }
+export -f sh_getcpu
 
 function sh_getmemory {
 	awk -F' ' 'NR==2 {print $2}' < <(free -h)
 }
+export -f sh_getmemory
 
 function sh_getvga {
 	awk -F: '/VGA/ {print $3}' < <(lspci)
 }
+export -f sh_getvga
 
 function sh_catecho {
 	echo "$(<"$1")"
 }
+export -f sh_catecho
 
 function sh_catprintf {
 	printf "%s" "$(<"$1")"
 }
+export -f sh_catprintf
 
 function sh_printfile {
 	sh_catecho "$1"
 }
+export -f sh_printfile
 
 function sh_installed_pkgs {
 	pacman -Q | cut -f1 -d" "
 }
+export -f sh_installed_pkgs
 
 function sh_info_msg {
 	#  echo -e "\033[1m$*\033[m"
 	printf '\033[1m%b\033[m' "$@"
 }
+export -f sh_info_msg
 
 function sh_get_greeting_message {
 	local -i hora_atual
@@ -145,23 +154,28 @@ function sh_get_greeting_message {
 	fi
 	echo "$greeting_message"
 }
+export -f sh_get_greeting_message
 
 function sh_get_user {
 	[[ "$USER" != "biglinux" ]] && echo " $USER"
 }
+export -f sh_get_user
 
 function sh_get_lang {
 	echo "$LANG"
 }
+export -f sh_get_lang
 
 function sh_get_locale {
 	grep _ <(locale -a) | head -1 | cut -c1-5
 }
+export -f sh_get_locale
 
 function sh_ignore_error {
 	"$@" 2>/dev/null
 	return 0
 }
+export -f sh_ignore_error
 
 function sh_div_lang {
 	if grep ^he <<<"$LANG"; then
@@ -170,6 +184,7 @@ function sh_div_lang {
 		echo '<div class="wrapper">'
 	fi
 }
+export -f sh_div_lang
 
 function info {
 	whiptail \
@@ -185,6 +200,7 @@ function info {
 	fi
 	return $result
 }
+export -f info
 
 function sh_splitarray {
 	local str=("$1")
@@ -205,6 +221,7 @@ function sh_splitarray {
 	IFS="$sep" read -r -a array <<<"${str[@]}"
 	echo "${array[pos - 1]}"
 }
+export -f sh_splitarray
 
 function sh_linuxHardware_run { sh_linuxHardware "$@"; }
 function sh_linuxhardware_run { sh_linuxHardware "$@"; }
@@ -214,6 +231,7 @@ function sh_linuxHardware {
 		xdg-open https://linux-hardware.org/?id="$*"
 	fi
 }
+export -f sh_linuxHardware
 
 function sh_replace_variables {
 	local text="$1"
@@ -224,12 +242,14 @@ function sh_replace_variables {
 	done
 	echo "$text"
 }
+export -f sh_replace_variables
 
 function sh_with_echo {
 	local HTML_CONTENT="$1"
 	local evaluated_text=$(sh_replace_variables "$HTML_CONTENT")
 	echo "$evaluated_text"
 }
+export -f sh_with_echo
 
 function sh_with_read {
 	local HTML_CONTENT
@@ -239,9 +259,88 @@ function sh_with_read {
 	local evaluated_text=$(sh_replace_variables "$HTML_CONTENT")
 	echo "$evaluated_text"
 }
+export -f sh_with_read
 
 function sh_with_cat {
 	cat <<-EOF
 		$(sh_replace_variables "$1")
 	EOF
 }
+export -f sh_with_cat
+
+function sh_diahora {
+	DIAHORA=$(date +"%d%m%Y-%T" | sed 's/://g')
+	printf "%s\n" "$DIAHORA"
+}
+export -f sh_diahora
+
+function sh_window_id {
+	xprop -root '\t$0' _NET_ACTIVE_WINDOW | cut -f 2
+}
+export -f sh_window_id
+
+function xdebug {
+#	yad --title="[debug]$0" --text="${*}\n" --width=400 --window-icon="$xicon" --button="Sim:1" --button="Não:2"
+#	result=$?
+#	(( result -eq 2 )) && exit 1
+#	return $result
+	kdialog --title "[debug]$0" --yesno "\n${*}\n" --icon dialog-information
+	result=$?
+	((result)) && exit 1
+	return $result
+}
+export -f xdebug
+
+function log_error {
+#	echo "Erro ocorreu no script: $1, linha: $2, erro: $3" | tee -i -a "$BOOTLOG" > "$LOGGER"
+#	echo "$1:$2] => $3" >> "$BOOTLOG"
+	printf "%30s:%-06d] : %s => %s\n" "$1" "$2" "$3 $4" >> "$BOOTLOG"
+}
+export -f log_error
+
+function cmdlogger {
+   local lastcmd="$@"
+   local line_number=${BASH_LINENO[0]}
+   local status
+   local error_output
+   local script_name="${0##*/}[${FUNCNAME[1]}]"
+
+   error_output=$( "$@" 2>&1 )
+#  status="${PIPESTATUS[0]}"
+   status="$?"
+   if [ $status -ne 0 ]; then
+      error_output=$(echo "$error_output" | cut -d':' -f3-)
+      log_error "$script_name" "$line_number" "$lastcmd" "$error_output"
+   fi
+   return $status
+}
+export -f cmdlogger
+
+function sh_kscreen_clean {
+	local xicon="$1"
+	local xtitle=$"Configurações da tela"
+	local xmsgbox=$"As configurações da tela foram resetadas."
+
+	cmdlogger rm -Rf ~/.local/share/kscreen
+#	kdialog --msgbox "$xmsgbox" --title "$xtitle" --icon "$xicon" &
+	yad --title="$xtitle" --text="\n$xmsgbox" --width=400 --window-icon="$xicon" --button="OK:0" &
+	sleep 5
+	wmctrl -c "$xtitle"
+}
+export -f sh_kscreen_clean
+
+function sh_reset_gwenview {
+	if pidof gwenview; then #program running
+		return
+	fi
+	cmdlogger rm -r ~/.local/share/kxmlgui5/gwenview
+	cmdlogger rm -r ~/.local/share/gwenview
+	cmdlogger rm ~/.config/gwenviewrc
+	if [[ "$1" = "skel" ]]; then
+		cmdlogger cp -r /etc/skel/.local/share/kxmlgui5/gwenview ~/.local/share/kxmlgui5/gwenview
+		cmdlogger cp -r /etc/skel/.local/share/gwenview ~/.local/share/gwenview
+		cmdlogger cp -f /etc/skel/.config/gwenviewrc ~/.config/gwenviewrc
+	fi
+	echo -n "#"
+}
+export -f sh_reset_gwenview
