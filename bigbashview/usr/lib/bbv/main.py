15509d91fdafccdfb356bf79b194a0ca4b9a88e3
Bruno Gonçalves <bigbruno@gmail.com>
2024-10-08 02:26:38 -0300

    Change flags for qtwebengine

--- bigbashview/usr/lib/bbv/main.py
+++ bigbashview/usr/lib/bbv/main.py
@@ -276,7 +276,7 @@ class Main:
             if args.gpu:
                 flags += (' --enable-gpu-rasterization')
             else:
-                flags += (' --disable-webgl --disable-accelerated-video-decode --disable-accelerated-video-encode --num-raster-threads=0 --enable-low-end-device-mode')
+                flags += (' --disable-webgl --disable-accelerated-video-decode --disable-accelerated-video-encode --num-raster-threads=0')
 
             # Verifica se a variável de ambiente QTWEBENGINE_CHROMIUM_FLAGS está vazia ou não definida
             if not os.environ.get('QTWEBENGINE_CHROMIUM_FLAGS', ''):
